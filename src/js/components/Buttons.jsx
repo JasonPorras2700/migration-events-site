7170aede026a3b35426417051d1d5c26f5b549cf
JasonPorras <140833791+JasonPorras2700@users.noreply.github.com>
2023-09-26 17:47:28 -0600

    update component

--- src/js/components/Buttons.jsx
+++ src/js/components/Buttons.jsx
@@ -1,24 +1,44 @@
-const MyComponent = ({ id, going, interested }) => {
+const GoingMessage = () => {
   return (
-    <div className="going-and-interested" data-id={id}>
-      <button
-        className="going"
-        data-id={id}
-        data-interaction={going}
-        data-template={going}
-      >
-        Going!
-      </button>
-      <button
-        className="interested"
-        data-id={id}
-        data-interaction={interested}
-        data-template={interested}
-      >
-        Interested
-      </button>
-    </div>
+    <>
+      <span className="check">✔</span>
+      <div>
+        <p>You`re going to this event!.</p>
+        <button className="button-link">Changed your mind?</button>
+      </div>
+    </>
   );
 };
 
-export {MyComponent};
+const InterestedMessage = () => {
+  return (
+    <>
+      <div>
+        <p>You`re interested in going.</p>
+        <button className="button-link">Changed your mind?</button>
+      </div>
+      <button className="going">Going!</button>
+    </>
+  );
+};
+
+const Buttons = () => {
+  const going = false;
+  const interested = false;
+
+  return (
+    <>
+      <div className="interactions-container">
+        <div className="going-and-interested">
+          <button className="going">Going!</button>
+          <button className="interested">Interested</button>
+        </div>
+        <button className="heart" aria-label="Favorite"></button>
+      </div>
+      {going ? <GoingMessage /> : null}
+      {interested ? <InterestedMessage /> : null}
+    </>
+  );
+};
+
+export { Buttons };
--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,20 +1,19 @@
-import { MyComponent } from "./Buttons.jsx";
-const GalleryCard = ({ imageUrl, eventName, date, location, price }) =>
-{
+import { Buttons } from "./Buttons.jsx";
+const GalleryCard = ({ imageUrl, eventName, date, location, price }) => {
   return (
-      <li className="gallery__card">
-        <img src={imageUrl} alt={eventName} />
-        <div className="gallery__text">
-          <div className="event__info">
-            <h3>{eventName}</h3>
-            <p className="date">{date}</p>
-            <p>{location}</p>
-            <strong>${price}</strong>
-            <MyComponent/>
-          </div>
+    <li className="gallery__card">
+      <img src={imageUrl} alt={eventName} />
+      <div className="gallery__text">
+        <div className="event__info">
+          <h3>{eventName}</h3>
+          <p className="date">{date}</p>
+          <p>{location}</p>
+          <strong>${price}</strong>
+          <Buttons />
         </div>
-      </li>
+      </div>
+    </li>
   );
-}
+};
 
-export {GalleryCard};
+export { GalleryCard };
